c2ef039dc0f0229281344ae015292c80c071d7f7
Anusha Konatala <anu.konatala@gmail.com>
2018-09-04 23:56:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,10 +24,43 @@ class Route{
   }
 }
 
-blocksTravelled(){
-  
-}
 
+  blocksTravelled() {
+    let startingHorizontal = eastWest.indexOf(this.beginningLocation.horizontal);
+    let endingHorizontal = eastWest.indexOf(this.endingLocation.horizontal);
+    let horizontalBlocks;
+    if (startingHorizontal > endingHorizontal) {
+      horizontalBlocks = startingHorizontal - endingHorizontal;
+    } else if (startingHorizontal < endingHorizontal) {
+      horizontalBlocks = endingHorizontal - startingHorizontal;
+    } else {
+      horizontalBlocks = 0;
+    }
+  let startingVertical = parseInt(this.beginningLocation.vertical);
+  let endingVertical = parseInt(this.endingLocation.vertical);
+  let verticalBlocks;
+
+  if (startingVertical > endingVertical) {
+    verticalBlockscks = startingVertical - endingVertical;
+  } else if (endingVertical > startingVertical) {
+      verticalBlocks = endingVertical - startingVertical;
+  } else {
+    verticalBlocks = 0;
+  }
+  return horizontalBlocks + verticalBlocks;
+  }
+  
+  estimatedTime() {
+    var d = new Date(); 
+    var hours = d.getHours();
+
+    if (hours >= 6 && hours < 10 || hours >=16 && hours < 20) {
+      return this.blocksTravelled(this.beginningLocation, this.endingLocation)/2;
+    }
+    else {
+      return this.blocksTravelled(this.beginningLocation, this.endingLocation)/3
+    }
+  }
 }
 
 
