c9d1c69c2f5dd0412ed7b0bf789f1ff59a63b67b
Anusha Konatala <anu.konatala@gmail.com>
2018-09-04 23:26:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 
+class Driver{
+	constructor(startDate,yearsExperienceFromBeginningOf ){
+this.startDate = new Date(startDate);
+}
+
+yearsExperienceFromBeginningOf(year){
+  return year - this.startDate;
+}
+
+}
+
+class Driver{
+	constructor(startDate,yearsExperienceFromBeginningOf ){
+this.startDate = new Date(startDate);
+}
+
+yearsExperienceFromBeginningOf(year){
+  return year - this.startDate;
+}
+
+}
+
